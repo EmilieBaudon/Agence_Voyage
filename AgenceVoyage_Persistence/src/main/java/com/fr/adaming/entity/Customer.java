7c8b7d2dd584bb67097a0f3a218ee40d590ce5d5
Nicolas Kervarec <kervarec.nicolas@gmail.com>
2019-02-15 16:25:42 +0100

    coms

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Customer.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Customer.java
@@ -9,6 +9,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 
+ *This class contains all the customers in the database. 
+ *It has attributes such as a bank card, and loyalty points, 
+ *acquired during a travel reservation.
+ *
+ * @author Nicolas
+ *
+ */
+
 @Entity
 @Getter
 @Setter
@@ -16,7 +26,14 @@ import lombok.ToString;
 @ToString
 public class Customer extends Person {
 
+	/**
+	 * @param Customer's card
+	 */
 	private String card;
+	
+	/**
+	 * @param Customer's fidelity points
+	 */
 	private Long fidelityPoint;
 
 	public Customer(String name, String firstName, Date birthDate, String adress, String mail, String pwd) {
