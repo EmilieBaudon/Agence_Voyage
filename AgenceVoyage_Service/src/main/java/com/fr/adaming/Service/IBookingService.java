bf2d73281c3fc6c184277767727398c4caac919f
EmilieBaudon <47633487+EmilieBaudon@users.noreply.github.com>
2019-02-15 16:17:26 +0100

    update IBookingService

--- AgenceVoyage_Service/src/main/java/com/fr/adaming/Service/IBookingService.java
+++ AgenceVoyage_Service/src/main/java/com/fr/adaming/Service/IBookingService.java
@@ -3,5 +3,11 @@ package com.fr.adaming.Service;
 import java.util.List;
 
 public interface IBookingService {
+  
+  public Booking create (Booking booking);
+	 public Booking update (Booking booking);
+	 public Booking readById (Long id);
+	 public List<Booking> readAll ();
+	
 
 }
