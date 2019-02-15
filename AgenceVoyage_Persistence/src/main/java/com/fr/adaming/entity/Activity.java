b9a48dd3af799e2781d19ae95c22d90bb55a8246
Quentin de Châteaubriant <quent043@hotmail.com>
2019-02-15 16:22:41 +0100

    Ajout de javaDoc sur les classes Activity Hotel et Standing

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Activity.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Activity.java
@@ -7,6 +7,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * This class summarizes all the activities that a traveler can do in the hotel he is staying.
+ * The activities a person can do depend on the Standing of the hotel this person is staying at.
+ * 
+ * @author Quentin
+ *
+ */
+
 @Entity
 @Getter
 @Setter
@@ -14,8 +22,20 @@ import lombok.ToString;
 @ToString
 public class Activity {
 
+	/**
+	 * @param Activity ID
+	 */
+	
 	Long id;
+	/**
+	 * @param  Activity Name
+	 */
 	String name;
+	
+	/**
+	 * @param Activity description
+	 */
+	
 	String desc;
 
 	public Activity(String name, String desc) {
