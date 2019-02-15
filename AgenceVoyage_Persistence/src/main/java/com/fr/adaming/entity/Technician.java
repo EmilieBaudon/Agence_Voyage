a934c4d152702d12f0c67ffd7026e57389b681eb
Nicolas Kervarec <47633433+NicolasKer@users.noreply.github.com>
2019-02-15 15:36:19 +0100

    Update Technician.java

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Technician.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Technician.java
@@ -1,5 +1,34 @@
 package com.fr.adaming.entity;
 
-public class Technician {
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Technician extends Person {
+
+	private String job;
+	private Date jobStartDate;
+
+	public Technician(String name, String firstName, Date birthDate, String adress, String mail, String pwd) {
+		super(name, firstName, birthDate, adress, mail, pwd);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Technician(String name, String firstName, Date birthDate, String adress, String mail, String pwd, String job,
+			Date jobStartDate) {
+		super(name, firstName, birthDate, adress, mail, pwd);
+		this.job = job;
+		this.jobStartDate = jobStartDate;
+	}
 
 }
