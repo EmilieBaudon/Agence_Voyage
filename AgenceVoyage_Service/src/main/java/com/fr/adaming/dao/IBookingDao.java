fa41024a787fac52bb4c5d510f0474f7ee50de13
EmilieBaudon <47633487+EmilieBaudon@users.noreply.github.com>
2019-02-18 09:40:50 +0100

    Delete IBookingDao.java

--- AgenceVoyage_Service/src/main/java/com/fr/adaming/dao/IBookingDao.java
+++
@@ -1,9 +0,0 @@
-package com.fr.adaming.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fr.adaming.entity.Activity;
-
-public interface IBookingDao extends JpaRepository<Booking, Long> {
-
-}
