96af786df825499761857c7f6f630837e4f1cb2b
Quentin de Châteaubriant <quent043@hotmail.com>
2019-02-18 10:25:55 +0100

    Modification classes Hotel et Standing

--- AgenceVoyage_Service/src/test/java/com/fr/adaming/service/HotelServiceTest.java
+++ AgenceVoyage_Service/src/test/java/com/fr/adaming/service/HotelServiceTest.java
@@ -3,6 +3,7 @@ package com.fr.adaming.service;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,12 +24,21 @@ public class HotelServiceTest {
 
 	
 	//Création d'un objet valide
+	@Test
 	public void a_createHotelTest() {
 		testHotel = new Hotel("Hilton", "hotel de riche");
 		service.create(testHotel);
 		assertNotNull(testHotel);
 		
-
 	}
+	
+	//Créer hotel deja existant
+	
+	//Créer un hotel avec ID nulle
+	
+	//Créer un hotel avec ID = 0
+	
+	//
+	
 
 }
