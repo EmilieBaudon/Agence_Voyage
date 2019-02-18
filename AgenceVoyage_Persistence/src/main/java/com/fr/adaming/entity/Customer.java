644822070c69abf82b4d2261917b8c7841ea8833
Karguel <47633708+Karguel@users.noreply.github.com>
2019-02-18 14:34:23 +0100

    Update Customer.java

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Customer.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/entity/Customer.java
@@ -1,6 +1,5 @@
 package com.fr.adaming.entity;
 
-<<<<<<< HEAD
 import java.time.LocalDate;
 import java.util.List;
 
@@ -9,11 +8,6 @@ import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-=======
-import java.util.Date;
-
-import javax.persistence.Entity;
->>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,14 +16,11 @@ import lombok.ToString;
 
 /**
  * 
-<<<<<<< HEAD
- * This class contains all the customers in the database. It has attributes such
- * as a bank card, and loyalty points, acquired during a travel reservation.
-=======
- *This class contains all the customers in the database. 
- *It has attributes such as a bank card, and loyalty points, 
- *acquired during a travel reservation.
->>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
+ * <<<<<<< HEAD This class contains all the customers in the database. It has
+ * attributes such as a bank card, and loyalty points, acquired during a travel
+ * reservation. ======= This class contains all the customers in the database.
+ * It has attributes such as a bank card, and loyalty points, acquired during a
+ * travel reservation. >>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
  *
  * @author Nicolas
  *
@@ -46,16 +37,12 @@ public class Customer extends Person {
 	 * @param Customer's card
 	 */
 	private String card;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
 	/**
 	 * @param Customer's fidelity points
 	 */
 	private Long fidelityPoint;
-<<<<<<< HEAD
+
 	/**
 	 * @param lbooking
 	 * @author Karguel
@@ -64,29 +51,17 @@ public class Customer extends Person {
 	@OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
 	List<Booking> lbooking;
 
-	public Customer(String name, String firstName, LocalDate birthDate, String adress, String mail, String pwd) {
-=======
-
-	public Customer(String name, String firstName, Date birthDate, String adress, String mail, String pwd) {
->>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
-		super(name, firstName, birthDate, adress, mail, pwd);
-		// TODO Auto-generated constructor stub
-	}
-
-<<<<<<< HEAD
 	public Customer(String name, String firstName, LocalDate birthDate, String adress, String mail, String pwd,
 			String card, Long fidelityPoint, List<Booking> lbooking) {
 		super(name, firstName, birthDate, adress, mail, pwd);
 		this.card = card;
 		this.fidelityPoint = fidelityPoint;
 		this.lbooking = lbooking;
-=======
-	public Customer(String name, String firstName, Date birthDate, String adress, String mail, String pwd,
-			String card, Long fidelityPoint) {
+	}
+
+	public Customer(String name, String firstName, LocalDate birthDate, String adress, String mail, String pwd) {
 		super(name, firstName, birthDate, adress, mail, pwd);
-		this.card = card;
-		this.fidelityPoint = fidelityPoint;
->>>>>>> ddc23f6f7023aa199ffcc6350af138b33ca34d81
+		// TODO Auto-generated constructor stub
 	}
 
 }
