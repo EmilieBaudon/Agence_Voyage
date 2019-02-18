77d0bc2ce4fdc78ab7428d2582b8d2900f02da3c
Emilie Baudon <emilie.baudon29@laposte.net>
2019-02-18 12:15:13 +0100

    Editing PersonService Test

--- AgenceVoyage_Service/src/test/java/com/fr/adaming/service/PersonServiceTest.java
+++ AgenceVoyage_Service/src/test/java/com/fr/adaming/service/PersonServiceTest.java
@@ -2,6 +2,7 @@ package com.fr.adaming.service;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fr.adaming.Service.IPersonService;
 import com.fr.adaming.entity.Person;
+
 /**
  * 
  * @author EmilieBaudon
@@ -24,64 +26,64 @@ import com.fr.adaming.entity.Person;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonServiceTest {
-	
+
 	@Autowired
 	@Qualifier("PersonService")
 	IPersonService service;
 	Person person;
-	
+
 	// Test create method
-	
+
 	@Test
 	public void a_InsertExistingUser() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		service.create(person);
 		person = service.readAll().get(0);
 		Person test = service.create(person);
 		assertNull(test);
 	}
-	
+
 	@Test
 	public void b_InsertUserWithNullId() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		person.setId(null);
 		assertNotNull(service.create(person));
 	}
-	
+
 	@Test
 	public void c_InsertUserWithIdEqualsZero() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		person.setId(0L);
 		assertNotNull(service.create(person));
 	}
-	
+
 	// Test update method
-	
+
 	@Test
 	public void d_InsertNonExistingUser() {
 
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		service.create(person);
-		person = service.readAll().get(service.readAll().size()-1);
-		person.setId(person.getId()+1);
+		person = service.readAll().get(service.readAll().size() - 1);
+		person.setId(person.getId() + 1);
 		assertNull(service.update(person));
 	}
-	
+
 	@Test
 	public void e_InsertNullId() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		person.setId(null);
 		assertNull(service.update(person));
 	}
-	
+
 	@Test
 	public void f_InsertIdEqualsZero() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
 		person.setId(0L);
 		assertNull(service.update(person));
 	}
-	
-	//Test readByMail method
+
+	// Test readByMail method
 
 	@Test
 	public void g_ReadUnexistingUser() {
@@ -90,24 +92,57 @@ public class PersonServiceTest {
 		Person test = service.readByEmail("nonexisting@email.com");
 		assertNull(test);
 	}
-	
+
 	// Test readById method
-
-	// Test readAll method
-
-	// Test deleteById method
 	
 	@Test
-	public void _UnexistingUser() {
-		person = new Person("test", "test", new Date(), "test", "test@test.fr", "test");
-		service.create(person);
-		person = service.readAll().get(service.readAll().size()-1);
-		}
+	public void h_ReadUnexistingUser() {
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
+		person = service.create(person);
+		person = service.readAll().get(service.readAll().size() - 1);
+		person.setId(person.getId() + 1);
+		Person test = service.readById(person.getId());
+		assertNull(test);
+	}
+
+	// Test readAll method
 	
+	@Test
+	public void i_ReadEmptyDB() {
+		
+		service.readAll();
+		
+	}
+
+	// Test deleteById method
+
+	@Test
+	public void j_UnexistingUser() {
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
+		person = service.create(person);
+		person = service.readAll().get(service.readAll().size() - 1);
+		person.setId(person.getId() + 1);
+		assertTrue(service.deleteById(person.getId()));
+	}
+
 	// Test deleteByMail method
+	
+	@Test
+	public void k_UnexistingUser() {
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
+		service.create(person);
+		assertTrue(service.deleteByEmail("nonexisting@email.com"));
+	}
 
 	// Test Login method
 	
+	@Test
+	public void l_UnexistingUser() {
+		person = new Person("test", "test", new Date(), "test", "test@test.com", "test");
+		person = service.create(person);
+		assertNull(service.Login("nonexisting@email.com", "notExistingPwd"));
+	}
+
 	@After
 	public void suppr() {
 		if (person != null && person.getId() != null && person.getId() != 0) {
