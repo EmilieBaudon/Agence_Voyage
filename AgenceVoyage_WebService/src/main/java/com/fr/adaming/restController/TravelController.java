e432a8def2f0d843469bf759f11c5b13e4ecc105
Nicolas Kervarec <kervarec.nicolas@gmail.com>
2019-02-18 17:17:14 +0100

    TravelController

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/restController/TravelController.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/restController/TravelController.java
@@ -1,15 +1,17 @@
 package com.fr.adaming.restController;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fr.adaming.Service.TravelService;
+import com.fr.adaming.dto.TravelDto;
 import com.fr.adaming.entity.Travel;
 
 /**
@@ -22,22 +24,24 @@ import com.fr.adaming.entity.Travel;
 
 @RestController
 @RequestMapping(path = "travel/")
-public class TravelController implements IController<Travel> {
+public class TravelController implements IController<TravelDto> {
 
+	/**
+	 * @param TravelService is an object used to access the database
+	 */
 	@Autowired
 	private TravelService service;
 
-	private Travel createdTravel;
+	private TravelDto dto;
 
 	@Override
 	@RequestMapping(path = "create", method = RequestMethod.POST)
-	public String createObject(@RequestBody Travel obj) {
+	public String createObject(@RequestBody TravelDto dto) {
 
-		createdTravel = new Travel(5, "Bora-Bora", LocalDate.of(2018, 06, 23), LocalDate.of(2018, 06, 28), null, null,
-				null);
-		createdTravel = service.create(createdTravel);
+		Travel travel = service.create(new Travel(dto.getNbrNight(), dto.getDestination(), dto.getPeriodBegin(),
+				dto.getPeriodEnd(), dto.getLbookingDto(), dto.getLflightDto(), dto.getHotelDto()));
 
-		if (createdTravel != null) {
+		if (travel != null) {
 
 			return "Travel has been created";
 		}
@@ -49,34 +53,47 @@ public class TravelController implements IController<Travel> {
 
 	@Override
 	@RequestMapping(path = "update", method = RequestMethod.POST)
-	public String updateObject(@RequestBody Travel obj) {
-		createdTravel = service.update(createdTravel);
+	public String updateObject(@RequestBody TravelDto obj) {
+		Travel travel = service.update(new Travel(dto.getNbrNight(), dto.getDestination(), dto.getPeriodBegin(),
+				dto.getPeriodEnd(), dto.getLbookingDto(), dto.getLflightDto(), dto.getHotelDto()));
+		if (travel != null) {
 
-		if (createdTravel != null) {
-			return "Travel has been created";
-		} else {
-			return "Travel has not been created";
+			return "Travel has been updated";
 		}
 
+		else {
+			return "Travel has not been updated";
+		}
 	}
 
 	@Override
 	@RequestMapping(path = "read/{id}", method = RequestMethod.GET)
-	public Travel readById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public TravelDto readById(@PathVariable(value = "id") Long id) {
+
+		Travel result = service.readById(id);
+		TravelDto dto = new TravelDto(result.getNbrNight(), result.getDestination(), result.getPeriodBegin(),
+				result.getPeriodEnd(), result.getLbooking(), result.getLflight(), result.getLflight());
+		return dto;
 	}
 
 	@Override
-	public List<Travel> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(path = "readall", method = RequestMethod.GET)
+	public List<TravelDto> readAll() {
+		List<Travel> result = service.readAll();
+		List<TravelDto> listDto = new ArrayList<TravelDto>();
+		for (Travel temp : result) {
+			listDto.add(new TravelDto(temp.getNbrNight(), temp.getDestination(), temp.getPeriodBegin(), temp.getPeriodEnd(), temp.getLbooking(), temp.getLflight(), temp.getHotel());
+		}
+		return listDto;
+		
+	
 	}
 
 	@Override
+	@RequestMapping(path = "delete/{id}", method = RequestMethod.DELETE)
 	public String delete(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		service.deleteById(id);
+		return "A flight has been delete";
 	}
 
 }
