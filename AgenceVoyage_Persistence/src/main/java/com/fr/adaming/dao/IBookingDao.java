d1540b67480942e3e58aa8bcb3c90696fa4025b1
gauchet <victor.gauchet@hotmail.com>
2019-02-18 10:57:40 +0100

    modif v2 dao dto

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/IBookingDao.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/IBookingDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fr.adaming.entity.Activity;
 import com.fr.adaming.entity.Booking;
-<<<<<<< HEAD
+
 /**
  * 
  * @author victor
@@ -13,9 +13,7 @@ import com.fr.adaming.entity.Booking;
 /**
  * This interface allows the persistence of data from the Class Booking
  */
-=======
 
->>>>>>> 70b84285ad8ec4bf8638575f9cbc19e31d1cf817
 public interface IBookingDao extends JpaRepository<Booking, Long> {
 
 }
--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/ITravelDao.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/ITravelDao.java
@@ -3,7 +3,7 @@ package com.fr.adaming.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fr.adaming.entity.Travel;
-<<<<<<< HEAD
+
 /**
  * 
  * @author victor
@@ -12,9 +12,7 @@ import com.fr.adaming.entity.Travel;
 /**
  * This interface allows the persistence of data from the Class Travel
  */
-=======
 
->>>>>>> 70b84285ad8ec4bf8638575f9cbc19e31d1cf817
 public interface ITravelDao extends JpaRepository<Travel, Long> {
 
 }
