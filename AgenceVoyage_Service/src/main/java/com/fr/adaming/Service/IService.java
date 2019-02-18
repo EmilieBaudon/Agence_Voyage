ea7c10ee06ca392391c74527bbfff0e63d35ea68
alan guihard <alan.guihard@2018.icam.fr>
2019-02-18 12:11:29 +0100

    Modification of the service

--- AgenceVoyage_Service/src/main/java/com/fr/adaming/Service/IService.java
+++ AgenceVoyage_Service/src/main/java/com/fr/adaming/Service/IService.java
@@ -2,14 +2,45 @@ package com.fr.adaming.Service;
 
 import java.util.List;
 
+
+/**
+ * 
+ * @author Alan
+ *
+ * @param <T> represent the classes service as Activity, Booking...
+ * 
+ */
 public interface IService<T> {
-	public T create(T activity);
+	/**
+	 * 
+	 * @param activity is an object representing the service
+	 * @return returns an object T that is return from the database at the creation of the object
+	 */
+	public T create(T object);
+	/**
+	 * 
+	 * @param activity is an object representing the service
+	 * @return returns an object T that is return from the database at the update of the object
+	 */
+	public T update(T object);
 
-	public T update(T activity);
-
+	/**
+	 * 
+	 * @param id is a Long representing the id  of the service
+	 * @return returns an object T that is return from the database using the parameter id
+	 */
 	public T readById(Long id);
 
+	/**
+	 * 
+	 * @return returns all the objects T  in a list of T that is return from the database 
+	 */
 	public List<T> readAll();
 
+	/**
+	 * 
+	 * @param id is a Long representing the id  of the service
+	 * @return return True if the delete haas worked and return false if it hasn't 
+	 */
 	public Boolean deleteById(Long id);
 }
---
+++ AgenceVoyage_Service/src/test/java/com/fr/adaming/service/FlightServiceTest.java
@@ -0,0 +1,136 @@
+package com.fr.adaming.service;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.NoSuchElementException;
+
+import org.junit.After;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fr.adaming.Service.FlightService;
+import com.fr.adaming.entity.Flight;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class FlightServiceTest {
+
+	@Autowired
+	private FlightService service;
+	
+	private Flight flight;
+
+	@Test
+	public void c_insertValid() {
+		flight = new Flight();
+		assertNotNull(service.create(flight));
+	}
+
+	@Test
+	public void d_insertExistingFlight() {
+		c_insertValid();
+		flight = service.readAll().get(0);
+		assertNull(service.create(flight));
+	}
+
+	@Test
+	public void a_insertFlightWithNullId() {
+		flight = new Flight();
+		flight.setId(null);
+		assertNotNull(service.create(flight));
+	}
+
+	@Test
+	public void b_insertFlightWithIdEqualsZero() {
+		flight = new Flight();
+		flight.setId(0L);
+		flight = service.create(flight);
+		assertNotNull(flight);
+	}
+
+	@Test
+	public void updateExistingFlight() {
+		c_insertValid();
+		flight = service.readAll().get(0);
+		assertNotNull(service.update(flight));
+	}
+
+	@Test
+	public void updateNotExistingFlight() {
+		flight = new Flight();
+		flight.setId(9999999L);
+		flight = service.update(flight);
+		assertNull(flight);
+	}
+
+	@Test
+	public void updateFlightWithIdNull() {
+		flight = new Flight();
+		flight.setId(null);
+		flight = service.update(flight);
+		assertNull(flight);
+	}
+
+	@Test
+	public void updateFlightWithIdEqualsZero() {
+		flight = new Flight();
+		flight.setId(0L);
+		flight = service.update(flight);
+		assertNull(flight);
+	}
+
+	@Test(expected = EmptyResultDataAccessException.class)
+	public void deleteNonExistingObject() {
+		service.deleteById(9999999L);
+	}
+
+	@Test
+	public void deleteExistingObject() {
+		c_insertValid();
+		flight = service.readAll().get(0);
+		try {
+			service.deleteById(flight.getId());
+			assertTrue(true);
+		} catch (Exception e) {
+			assertFalse(true);
+		} finally {
+			flight = new Flight();
+		}
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void readNonExistingObject() {
+		flight = service.readById(9999999L);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void readAllEmptyTable() {
+		flight = service.readAll().get(0);
+	}
+
+	@Test
+	public void readExistingObject() {
+		c_insertValid();
+		flight = service.readAll().get(0);
+		flight = service.readById(flight.getId());
+		assertNotNull(flight);
+	}
+
+	@After // méthode sera appelé après chaque méthode de tests
+	public void after() {
+		System.out.println("************************DEBUG TESTING AfterMethod***********************");
+		if (flight != null && flight.getId() != null) {
+			service.deleteById(flight.getId());
+		}
+	}
+}
