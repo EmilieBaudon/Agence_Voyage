0594318df1962133fd3b9f9ce311a3ab9794d1fd
Emilie Baudon <emilie.baudon29@laposte.net>
2019-02-19 14:23:46 +0100

    Adding person rest controller

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/dto/TechnicianDto.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/dto/TechnicianDto.java
@@ -3,6 +3,9 @@ package com.fr.adaming.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
@@ -24,6 +27,36 @@ import lombok.Setter;
 
 public class TechnicianDto {
 
+	/**
+	 * @param Person name
+	 */
+	@NotNull
+	private String name;
+	/**
+	 * @param Person first name
+	 */
+	@NotNull
+	private String firstName;
+	/**
+	 * @param Person date of birth
+	 */
+	@NotNull
+	@JsonFormat(pattern = "dd-MM-yyyy", shape = JsonFormat.Shape.STRING, timezone="CET")
+	private LocalDate birthDate;
+	/**
+	 * @param Person adress
+	 */
+	@NotNull
+	private String adress;
+	/**
+	 * @param Person mail
+	 */
+	@Email
+	private String mail;
+	/**
+	 * @param Person password
+	 */
+	private String pwd;
 	/**
 	 * @param Technician's job
 	 */
