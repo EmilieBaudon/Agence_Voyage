b09e1e45daa380f6868ba4ed8546f8b4deca5107
Nicolas Kervarec <kervarec.nicolas@gmail.com>
2019-02-19 10:05:38 +0100

    Import solved problems

--- AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/IStandingDao.java
+++ AgenceVoyage_Persistence/src/main/java/com/fr/adaming/dao/IStandingDao.java
@@ -2,7 +2,6 @@ package com.fr.adaming.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.fr.adaming.entity.Activity;
 import com.fr.adaming.entity.Standing;
 /**
  * 
