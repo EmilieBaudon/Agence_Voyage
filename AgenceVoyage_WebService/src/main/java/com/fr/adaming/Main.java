bdea3a2fce4d4be32a26f918589fea5db1e073e3
alan guihard <alan.guihard@2018.icam.fr>
2019-02-19 10:50:19 +0100

    Modification of the main to allow swagger to work

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
@@ -3,7 +3,10 @@ package com.fr.adaming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Main {
 
 	public static void main(String[] args) {
