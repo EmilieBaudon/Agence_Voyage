8ae8ab091004aa92f3350ce862ae8c697f3f2cdd
gauchet <victor.gauchet@hotmail.com>
2019-02-19 14:57:59 +0100

    log4j example :service+controller

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
@@ -1,5 +1,6 @@
 package com.fr.adaming;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +12,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+		
+		
 
 	}
-
+	
 }
