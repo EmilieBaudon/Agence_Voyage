6aafcac9adc3e01c8b8113334e4da544a9aa7da2
gauchet <victor.gauchet@hotmail.com>
2019-02-20 12:39:09 +0100

    activity test amélioré

--- AgenceVoyage_WebService/src/test/java/com/fr/adaming/service/ActivityServiceTest.java
+++ AgenceVoyage_WebService/src/test/java/com/fr/adaming/service/ActivityServiceTest.java
@@ -3,36 +3,45 @@ package com.fr.adaming.service;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.fr.adaming.Service.ActivityService;
+import com.fr.adaming.Service.FlightService;
 import com.fr.adaming.entity.Activity;
+import com.fr.adaming.entity.Flight;
+import com.fr.adaming.entity.Person;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 /**
  * 
- * @author victo
+ * @author victor
  *
  */
 public class ActivityServiceTest {
 	@Autowired
 	private ActivityService activityService;
 
+	private Activity activity;
+	
 	@Test
 	public void createActivityTest() {		
 		 /** 
 		  *  test the insertion of an activity which already exist in database
 		  */
 		Activity activity = new Activity("activity", "test");
-		Activity result = activityService.create(activity);
-		assertNotNull(result);
+		activity  = activityService.create(activity);
+		assertNotNull(activity);
 
 	}
 
@@ -55,12 +64,10 @@ public class ActivityServiceTest {
 		/** 
 		 * test the modification of an activity which already exist
 		 */
-		
-		Activity activity = new Activity("adaming", "test");
-		Activity result = activityService.create(activity);
-		activity.setName("activityUpdate");
-		activity = activityService.update(activity);
-		assertNotNull(activity);
+		Activity activity = new Activity("activity", "test");
+		activity  = activityService.create(activity);
+		activity = activityService.readAll().get(0);		
+		assertNotNull(activityService.update(activity));
 	}
 	
 	@Test
@@ -70,6 +77,44 @@ public class ActivityServiceTest {
 		 */
 		Boolean objDelete = activityService.deleteById(9999999L);
 		assertFalse(objDelete);
+	}	
+
+	@Test(expected = NoSuchElementException.class)
+	public void readNonExistingObject() {
+		/** 
+		 * test the read of an activity which doesn't exist
+		 */
+		activity = activityService.readById(9999999L);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void readAllEmptyTable() {
+		/** 
+		 * test the read of an list of activity which is empty
+		 */
+		activity = activityService.readAll().get(0);
+	}
+
+	@Test
+	public void readExistingObject() {
+		/** 
+		 * test the read of an activity which exist
+		 */
+		Activity activity = new Activity("adaming", "test");
+		Activity result = activityService.create(activity);
+		activity = activityService.readAll().get(0);
+		activity = activityService.readById(activity.getId());
+		assertNotNull(activity);
+	}
+
+	@After
+	public void suppr() {
+		List <Activity> activities = activityService.readAll();
+		if (activities != null) {
+			for (int i=0; i<activities.size();i++) {
+				activityService.deleteById(activities.get(i).getId());
+			}
+		}
 	}
 
 }
