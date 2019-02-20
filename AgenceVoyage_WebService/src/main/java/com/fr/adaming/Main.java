886074840e6b949b4257effce53c027d9c47d754
Emilie Baudon <emilie.baudon29@laposte.net>
2019-02-20 12:48:00 +0100

    main

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
@@ -1,32 +1,15 @@
 package com.fr.adaming;
 
-import org.apache.log4j.Logger;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fr.adaming.Service.ActivityService;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 
 @SpringBootApplication
 @EnableSwagger2
 public class Main {
 
 	public static void main(String[] args) {
-		
-		final Logger log = Logger.getLogger(Main.class);
-		
-		try {		
-		SpringApplication.run(Main.class, args);}
-		
-		finally{
-		log.fatal("the application failed");		
-		}
-		
-		
 
 	}
-	
+
 }
