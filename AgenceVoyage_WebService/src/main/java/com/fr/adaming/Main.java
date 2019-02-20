cfb04e758f93908bf8a1f8e24267ea8d971cda5f
alan guihard <alan.guihard@2018.icam.fr>
2019-02-20 15:56:58 +0100

    Delete try catch from main

--- AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
+++ AgenceVoyage_WebService/src/main/java/com/fr/adaming/Main.java
@@ -1,6 +1,5 @@
 package com.fr.adaming;
 
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Main {
 
 	public static void main(String[] args) {
-		final Logger log = Logger.getLogger(Main.class);
 
-		try {
-			SpringApplication.run(Main.class, args);
-		} catch (Exception e) {
-			log.fatal("the application failed");
-		}
+		SpringApplication.run(Main.class, args);
 	}
 }
